4c15959bde004649ea699eb457e0d0b7d0a071d9
Helene Thomsen <helenet@live.dk>
2024-01-12 00:05:19 +0100

    added navbar in js to make sure it is included in all my files

---
+++ navbar.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var navbarHTML = `
+    <header class="main-header">
+        <div class="main-header-logo">
+            <img src="https://helenethomsen.github.io/HelenesHobbies/images/logo/logo_dog_and_yarn_v1.png" alt="logo">
+        </div>
+        <nav class="main-header-nav">
+            <ul>
+                <li><a href="index.html">HOME</a></li>
+                <li><a href="#">ONGOING</a></li>
+                <li><a href="#">KNITTING</a></li>
+                <li><a href="/crochet/lego_keychains/lego_keychain.html">CROCHET</a></li>
+                <li><a href="#">SHUTTLE<br> TATTING</a></li>
+                <li><a href="#">TEABAG<br>FOLDING</a></li>
+            </ul>
+            </nav>
+            <div class="main-header-sm">
+            <a href="#"><div class="main-header-sm-fb"></div></a>
+            <a href="#"><div class="main-header-sm-in"></div></a>
+        </div>
+    </header>
+    `;
+    document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+});
